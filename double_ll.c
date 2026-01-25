ab3f8d6175b611e737455e8afdfb06fddba5738c
vatsal <153715547+vatsal633@users.noreply.github.com>
2026-01-25 10:03:14 +0530

    Refactor double linked list functions and structure

--- double_ll.c
+++ double_ll.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 
-struct node{
+struct node
+{
     int data;
     struct node *prev;
     struct node *next;
@@ -10,66 +11,121 @@ struct node{
 
 struct node *head;
 struct node *tail;
+int size = 0;
 
-struct node *create(){
+struct node *create(int val)
+{
     struct node *newNode;
-    newNode = (struct node*) malloc(sizeof(struct node));
+    newNode = (struct node *)malloc(sizeof(struct node));
 
-    if(newNode==NULL){
+    if (newNode == NULL)
+    {
         printf("allocation failed!\n");
         return 0;
     }
-
-    return newNode;
-}
-
-void push_back(){
-    int val;
-    struct node *newNode = create();
-
-    printf("enter the value");
-    scanf("%d",&val);
 
     newNode->data = val;
     newNode->prev = NULL;
     newNode->next = NULL;
 
-    if(head==NULL){
+    return newNode;
+}
+
+void push_back(int val)
+{
+    struct node *newNode = create(val);
+
+    if (head == NULL)
+    {
         head = tail = newNode;
-    }else{
+        size++;
+    }
+    else
+    {
         tail->next = newNode;
         newNode->prev = tail;
         tail = newNode;
+        size++;
     }
 }
 
+void push_front(int val)
+{
+    struct node *newNode = create(val);
 
-void display(){
+    if (head == NULL)
+    {
+        head = tail = newNode;
+        size++;
+    }
+    else
+    {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        size++;
+    }
+}
+
+void insertAt(int val, int pos)
+{
+    struct node *newNode = create(val);
     struct node *temp = head;
-    if (temp==NULL)
+    int i = 0;
+
+    if(pos==0){
+        push_front(val);
+        return;
+    }
+
+    if (pos>size)
+    {
+        push_back(val);
+    }
+    
+    
+    while (temp != NULL && i != pos - 1);
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    temp->next->prev = newNode;
+    temp->next = newNode;
+}
+
+void display()
+{
+    struct node *temp = head;
+    if (temp == NULL)
     {
         printf("the list is empty\n");
         return;
     }
-    
-    while (temp!=NULL)
+
+    while (temp != NULL)
     {
-        if(temp->next==NULL){
-            printf("%d",temp->data);
+        if (temp->next == NULL)
+        {
+            printf("%d", temp->data);
         }
-        else{
-            printf("%d <->",temp->data);
+        else
+        {
+            printf("%d <->", temp->data);
         }
         temp = temp->next;
     }
-    
 }
-void main(){
-    push_back();
-    push_back();
-    push_back();
-    push_back();
-    push_back();
+
+void main()
+{
+    push_front(1);
+    push_front(2);
+    push_front(3);
+    push_front(4);
     display();
     getch();
 }
